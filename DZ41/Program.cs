fb83cbec09d902adfca980371378cb03c0aad8af
Alexandr <kazancevalex1987@gmail.com>
2022-10-22 19:02:58 +1200

    Домашнее задание 41

---
+++ DZ41/Program.cs
@@ -0,0 +1,16 @@
+﻿// Домашнее задание 41
+
+Console.Clear();
+Console.Write("Введите элементы(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов > 0: {count}");
