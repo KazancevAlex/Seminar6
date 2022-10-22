334c32af03abcc607cefc8dea8645760c6ab862b
Alexandr <kazancevalex1987@gmail.com>
2022-10-22 20:50:56 +1200

    Домашнее задание 41

--- DZ41/Program.cs
+++ DZ41/Program.cs
@@ -1,16 +1,34 @@
 ﻿// Домашнее задание 41
 
 Console.Clear();
-Console.Write("Введите элементы(через пробел): ");
-int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-int count = 0;
- 
-for (int i = 0; i < arr.Length; i++)
+Console.Write("сколько чисел будет введено: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+void InputArray(int[] array)
 {
-    if (arr[i] > 0)
-    {
-        count++;
-    }
+    for (int i = 0; i < array.Length; i++)
+        array[i] = Convert.ToInt32(Console.ReadLine());
+
 }
- 
-Console.WriteLine($"Кол-во элементов > 0: {count}");
+
+void PrintArray(int[] array)
+{
+    Console.Write($"[{string.Join(",", array)}]");
+}
+
+int[] arr = new int[M];
+Console.WriteLine("Введите числа: ");
+InputArray(arr);
+PrintArray(arr);
+
+void PositiveNumber(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    if (array[i] > 0)
+    count++;
+    Console.WriteLine($"Количество чисел больше нуля = {count}");
+}
+
+Console.WriteLine();
+PositiveNumber(arr);
